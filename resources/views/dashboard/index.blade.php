130f984ccdafc626b567f2227c65f63020324a67
AliKhalifa <ali689.kh@gmail.com>
2021-01-05 12:45:11 +0200

    adding languages in url

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.dashboard.app')
+
+
+@section('content')
+    
+    <div class="content-wrapper">
+        <section class="content-header">
+            <h1>
+                Blanck Bage
+                <small>it all startes here</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li class="active"><i class="fa fa-dashboard"></i> @lang('site.dashboard')</li>
+            </ol>
+            
+        </section>
+        <section class="content">
+            <h1>
+                this is ali
+            </h1>
+        </section>
+    </div>
+
+@endsection
