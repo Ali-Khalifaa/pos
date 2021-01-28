28501a723bfb5ee427869babd59a3f7edb55d7dc
AliKhalifa <ali689.kh@gmail.com>
2021-01-28 16:40:15 +0200

    finalizing dash

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -4,12 +4,32 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Client;
+use App\Models\Order;
+use Illuminate\Support\Facades\DB;
+
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index');
+        $products_count=Product::count();
+        $categories_count=Category::count();
+        $clients_count=Client::count();
+        $users_count=User::whereRoleIs('admin')->count();
+
+
+        $sales_data = Order::select(
+            Db::raw('YEAR (created_at) as year'),
+            Db::raw('MONTH (created_at) as month'),
+            Db::raw('SUM(total_price) as sum'),
+        )->groupBy('month')->get();
+
+
+        return view('dashboard.index',compact('products_count','categories_count','clients_count','users_count','sales_data'));
         
     }//end of index
     
